c5cb5f465280aa4e0f93324b93a8e6204495b7ea
smallengines <phil@smallenginerepair-usa.com>
2025-05-17 22:04:12 -0400

    Delete terms.js

--- terms.js
+++
@@ -1,8 +0,0 @@
-export default function Terms() {
-  return (
-    <main className="p-6 max-w-xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Terms & Conditions</h1>
-      <p>Terms placeholder content here.</p>
-    </main>
-  );
-}
